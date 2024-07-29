38f7844eb4d4928ff0686eb5dc14862e4d9428c7
omartabbaa <omartabbaa0@gmail.com>
2024-07-29 14:26:18 +0200

    Update exercises.sql

--- SQL Deep Dive/Aggregate Functions/exercises.sql
+++ SQL Deep Dive/Aggregate Functions/exercises.sql
@@ -8,11 +8,11 @@
 
 -- Question 1: What is the average salary for the company?
 -- Table: Salaries
-
+63810.744836143706
 
 -- Question 2: What year was the youngest person born in the company?
 -- Table: employees
-
+1965-02-01
 
 -- BEFORE YOU START
 /*
@@ -23,7 +23,7 @@
 
 -- Question 1: How many towns are there in france?
 -- Table: Towns
-
+36684
 
 -- BEFORE YOU START
 /*
@@ -34,11 +34,11 @@
 
 -- Question 1: How many official languages are there?
 -- Table: countrylanguage
-
+984
 -- Question 2: What is the average life expectancy in the world?
 -- Table: country
-
+66.486036
 -- Question 3: What is the average population for cities in the netherlands?
 -- Table: city
-
+185001.75
 
